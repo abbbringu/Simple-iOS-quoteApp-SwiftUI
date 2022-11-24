baeaf87c4b5483039a218e7f011a8b1457a201af
abbbringu <brianphi.nguyen@gmail.com>
2022-11-24 21:26:23 +0100

    Initial Commit

---
+++ Final module 2/Final_module_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Final_module_2App.swift
+//  Final module 2
+//
+//  Created by Brian Nguyen on 2022-11-24.
+//
+
+import SwiftUI
+
+@main
+struct Final_module_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
